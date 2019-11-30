1dedb0071c93a0347f89f30b28c3fa9ba93fe40a
Saadat Mateen <saadat@saadatmand.pk>
2019-11-30 22:47:27 +0500

    Add 'U+XXXX <character name>' information to keyboards and tables

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -12,6 +12,22 @@ const DEST = './build';
 
 // environment function for nunjucks
 var env = function(environment) {
+    var categories = [
+        "Cc Cf Co Cs",
+        "Ll Lm Lo Lt Lu",
+        "Mc Me Mn",
+        "Nd Nl No",
+        "Pc Pd Pe Pf Pi Po Ps",
+        "Sc Sk Sm So",
+        "Zl Zp Zs",
+    ].join(' ').split(' ');
+
+    var unicodeData = {};
+
+    categories.forEach(function (cat) {
+        unicodeData = Object.assign(unicodeData, require('unicode/category/' + cat));
+    });
+        
     environment.addGlobal('char_categories', {
         "alphabets": "آ ا أ ب پ ت ٹ ث ج چ ح خ د ڈ ذ ر ڑ ز ژ س ش ص ض ط ظ ع غ ف ق ک گ ل م ن ں و ؤ ہ ۂ ۃ ھ ء ئ ی ے ۓ",
         "digits": "۰ ۱ ۲ ۳ ۴ ۵ ۶ ۷ ۸ ۹ 0 1 2 3 4 5 6 7 8 9",
@@ -25,6 +41,16 @@ var env = function(environment) {
         return str.split(' ');
     });
     
+    environment.addFilter('uniname', function(str) {
+        if (str == "RLM") return "U+200F RIGHT-TO-LEFT MARK";
+        if (str == "LRM") return "U+200E LEFT-TO-RIGHT MARK";
+        if (str == "ZWJ") return "U+200D ZERO WIDTH JOINER";
+        if (str == "")    return "";
+        
+        codepoint = unicodeData[str.charCodeAt(0)].value;
+        name = unicodeData[str.charCodeAt(0)].name;
+        return `U+${codepoint} ${name}`;
+    });
 }
 
 function pages () {

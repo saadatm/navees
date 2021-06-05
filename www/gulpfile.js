82093ba4cef9f21e3ce670c5b655bea69d4eb1d5
Saadat Mateen <saadat@saadatmand.pk>
2021-06-05 21:22:56 +0500

    Use @unicode/unicode-13.0.0 for 'U+XXXX <character name>' information

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -6,28 +6,13 @@ const gulp = require('gulp');
 const nunjucks = require('gulp-nunjucks-render');
 const postcss = require('gulp-postcss');
 const prettify = require('gulp-prettify');
+const unicodeNames = require('@unicode/unicode-13.0.0/Names/index.js');
 
 const SRC = '.';
 const DEST = './build';
 
 // environment function for nunjucks
 var env = function(environment) {
-    var categories = [
-        "Cc Cf Co Cs",
-        "Ll Lm Lo Lt Lu",
-        "Mc Me Mn",
-        "Nd Nl No",
-        "Pc Pd Pe Pf Pi Po Ps",
-        "Sc Sk Sm So",
-        "Zl Zp Zs",
-    ].join(' ').split(' ');
-
-    var unicodeData = {};
-
-    categories.forEach(function (cat) {
-        unicodeData = Object.assign(unicodeData, require('unicode/category/' + cat));
-    });
-        
     environment.addGlobal('char_categories', {
         "alphabets": "آ ا أ ب پ ت ٹ ث ج چ ح خ د ڈ ذ ر ڑ ز ژ س ش ص ض ط ظ ع غ ف ق ک گ ل م ن ں و ؤ ہ ۂ ۃ ھ ء ئ ی ے ۓ",
         "digits": "۰ ۱ ۲ ۳ ۴ ۵ ۶ ۷ ۸ ۹ 0 1 2 3 4 5 6 7 8 9",
@@ -36,19 +21,19 @@ var env = function(environment) {
         "honorifics": "ﷻ ؐ ﷺ ؑ ؓ ؒ ؔ",
         "misc": "﷽ ZWNJ ZWJ RLM LRM ` ~ + = _ | @ # $ ٪ ^ & * < >"
     });
-    
+
     environment.addFilter('split', function(str) {
         return str.split(' ');
     });
-    
+
     environment.addFilter('uniname', function(str) {
         if (str == "RLM") return "U+200F RIGHT-TO-LEFT MARK";
         if (str == "LRM") return "U+200E LEFT-TO-RIGHT MARK";
         if (str == "ZWJ") return "U+200D ZERO WIDTH JOINER";
         if (str == "")    return "";
         
-        codepoint = unicodeData[str.charCodeAt(0)].value;
-        name = unicodeData[str.charCodeAt(0)].name;
+        codepoint = str.charCodeAt(0).toString(16).padStart(4, 0).toUpperCase();
+        name = unicodeNames.get(str.charCodeAt(0));
         return `U+${codepoint} ${name}`;
     });
 }
